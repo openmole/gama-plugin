65de388913bf75593f6f4758f77d450fb071dfe8
rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2019-07-16 12:19:51 +0200

    change way to ci build

--- build.sh
+++ build.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
 
-cd openmole
-
-git lfs fetch
-
-cd build-system && sbt clean publishLocal
-cd ..
-cd libraries && sbt clean publishLocal
-cd ..
-cd openmole && sbt clean publishLocal
-
-cd ..
-
 ./gradlew assemble
