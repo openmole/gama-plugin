5a319054fb71f44dd20e5913b284d43fe7241314
rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2019-07-16 12:08:45 +0200

    test gitlab submodule option

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 cd openmole
 
-git submodule init
-git submodule update
 git lfs fetch
 
-(cd build-system && sbt clean publishLocal)
-(cd libraries && sbt clean publishLocal)
-(cd openmole && sbt clean publishLocal)
+cd build-system && sbt clean publishLocal
+cd ..
+cd libraries && sbt clean publishLocal
+cd ..
+cd openmole && sbt clean publishLocal
 
 cd ..
 
