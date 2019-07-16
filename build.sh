a9d2c78bdfdc0848272cf627ac2ce0febb84b9c8
rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2019-07-16 11:57:18 +0200

    modify build.sh to compile openmole before gradle exec

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+cd openmole
+
+git submodule init
+git submodule update
+git lfs fetch
+
+(cd build-system && sbt clean publishLocal)
+(cd libraries && sbt clean publishLocal)
+(cd openmole && sbt clean publishLocal)
+
+cd ..
+
 gradle assemble
